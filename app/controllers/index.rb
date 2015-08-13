e4375e58410ef7e8600cd648cc8f02544be6a410
ksliu25 & MatthewGreenberg <pair+ksliu25.matthewgreenberg@devbootcamp.com>
2015-08-13 15:03:47 -0500

    Created backend logic for freq and avg score

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,5 @@
+
+
 get '/' do
   # Look in app/views/index.erb
   @comments = Comment.all
@@ -9,3 +11,30 @@ post '/' do
   @comments = Comment.all
   @comements.to_json
 end
+
+post '/data' do
+  content_type :json
+  @words = params.split(" ")
+  array = []
+  @words.each do |word|
+    hash = {}
+    @comments = Comment.frequencies(word)
+    hash[word] = word
+    hash[frequency] = @comments.count
+    hash[avgpoints] = Comment.avgpoints(@comments)
+    array << hash
+  end
+  array.to_json
+end
+
+
+=begin
+[{word: "dog", frequency: 10, guilded: 3, avg: 20}, {word: "cat", freq: 10, guilded: 3}]
+
+array = []
+
+
+
+
+
+=end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,12 @@
 class Comment < ActiveRecord::Base
+
+  def self.frequencies(word)
+    self.where("body LIKE ?", "%#{word}%")
+  end
+
+  def self.avgpoints(array_containing_comments)
+    array_containing_comments.average('score').to_i
+    # array_containing_comments.map(&:score).inject(:+) / array_containing_comments.count
+  end
+
 end
